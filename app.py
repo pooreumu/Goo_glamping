3b56733e02cce8714801eca0bd554362fb1d0593
rlasuddn <rlasuddn>
2022-03-08 23:04:58 +0900

    1차완성본

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import config
+import hashlib
 app = Flask(__name__)
 
 from pymongo import MongoClient
@@ -10,20 +11,33 @@ db = client.dbsparta
 def home():
     return render_template('index.html')
 
+@app.route('/signup')
+def sign_up():
+    return render_template('sign_up.html')
 
-# @app.route("/", methods=[""])
-# def ():
-#
-#
-#     sample_receive = request.form['']
-#     return jsonify({'': ''})
+@app.route('/signup/save', methods=['POST'])
+def sign_up_save():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+    pwcheck_receive = request.form['pwcheck_give']
+    print(id_receive, pwcheck_receive, pwcheck_receive)
+    # 패스워드 일치하는지 검사
+    if pw_receive == pwcheck_receive:
+        pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+        db.users.insert_one({'id': id_receive, 'pw': pw_hash})
+        return jsonify({'result': 'success', 'msg': '회원가입이 완료되었습니다.'})
+    elif pw_receive != pwcheck_receive:
+        return jsonify({'result': 'fail', 'msg': '패스워드가 일치 하지 않습니다.'})
 
-# doc = {
-#        데이터
-#     }
-#
-#     db.(각자 필요한 폴더 이름).insert_one(doc)
-
+@app.route('/signup/idcheck', methods=['POST'])
+def sign_up_idcheck():
+    # 아이디중복검사
+    id_receive = request.form['id_give']
+    result = db.users.find_one({'id': id_receive}, {"_id": False})
+    if result is not None:
+        return jsonify({'result': 'fail', 'msg': '중복된 아이디가 존재합니다.'})
+    elif result is None:
+        return jsonify({'result': 'success', 'msg': '아이디 중복체크완료!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
