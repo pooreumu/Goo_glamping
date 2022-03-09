a01dec560c74ca9da28986b93e8a7135087b5bc4
DevTaaki <kjtag0408@gmail.com>
2022-03-09 18:05:34 +0900

    image 업로드 기능 추가 전 merge를 위한 commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
-from flask import Flask, render_template, request, jsonify
-from datetime import datetime, timedelta
+
+from flask import Flask, render_template, request, jsonify, url_for, redirect
+
 import config
 import jwt
 from datetime import datetime, timedelta
@@ -13,14 +14,16 @@ db = client.dbsparta
 
 SECRET_KEY = config.SECRET_KEY
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
 ### sign_up api ###
 @app.route('/signup')
 def sign_up():
     return render_template('sign_up.html')
-
 
 
 @app.route('/signup/save', methods=['POST'])
@@ -37,6 +40,7 @@ def sign_up_save():
     elif pw_receive != pwcheck_receive:
         return jsonify({'result': 'fail', 'msg': '패스워드가 일치 하지 않습니다.'})
 
+
 @app.route('/signup/idcheck', methods=['POST'])
 def sign_up_idcheck():
     # 아이디중복검사
@@ -46,6 +50,7 @@ def sign_up_idcheck():
         return jsonify({'result': 'fail', 'msg': '중복된 아이디가 존재합니다.'})
     elif result is None:
         return jsonify({'result': 'success', 'msg': '아이디 중복체크완료!'})
+
 
 ### login api #####
 @app.route('/sign_in', methods=['POST'])  # 로그인 API
@@ -64,23 +69,24 @@ def sign_in():
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')  # 토큰을 건내줌.
 
-
         return jsonify({'result': 'success', 'token': token})
     else:  # 동일한 유저가 없으면,
         return jsonify({'result': 'fail', 'msg': '아이디/패스워드가 일치하지 않습니다.'})
-      
+
+
 ###top10###
 @app.route('/top10')
 def top10():
     return render_template('top10.html')
 
 
-
 ### review_list api ###
+
 
 @app.route('/main')
 def main():
     return render_template('review_list.html')
+
 
 @app.route('/main/post', methods=['POST'])
 def review_post():
@@ -91,7 +97,6 @@ def review_post():
 
     doc = {
         'title':title_receive,
-        # 이미지도 필요!
         'loc':loc_receive,
         'star':star_receive,
         'review':review_receive
@@ -101,18 +106,33 @@ def review_post():
 
     return jsonify({'msg':'저장완료!'})
 
+
 @app.route('/main/get', methods=['GET'])
 def review_get():
     review_list = list(db.reviews.find({}, {'_id': False}))
     return jsonify({'reviews':review_list})
 
 
-
 @app.route('/top10/api', methods=['GET'])
 def top10_api():
-    top10_list = list(db.top10.find({},{'_id':False}))
-    print(top10_list)
+    top10_list = list(db.top10.find({}, {'_id': False}))
     return jsonify({'top10': top10_list})
+
+
+################
+
+@app.route('/main')
+def main_main():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"userid": payload["id"]})
+
+        return render_template('review_list.html', user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
 
 if __name__ == '__main__':
