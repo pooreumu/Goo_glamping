aa45a7c04d38c953d26b982899c06701812a9c25
rlasuddn <rlasuddn>
2022-03-09 12:06:00 +0900

    2차 완성본

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from pymongo import MongoClient
 from flask import Flask, render_template, request, jsonify
 import config
 import jwt
@@ -5,7 +6,6 @@ from datetime import datetime, timedelta
 import hashlib
 app = Flask(__name__)
 
-from pymongo import MongoClient
 client = MongoClient(config.Mongo_key)
 db = client.dbsparta
 
@@ -14,7 +14,7 @@ SECRET_KEY = config.SECRET_KEY
 @app.route('/')
 def home():
     return render_template('home.html')
-
+### sign_up api ###
 @app.route('/signup')
 def sign_up():
     return render_template('sign_up.html')
@@ -24,7 +24,7 @@ def sign_up_save():
     id_receive = request.form['id_give']
     pw_receive = request.form['pw_give']
     pwcheck_receive = request.form['pwcheck_give']
-    print(id_receive, pwcheck_receive, pwcheck_receive)
+
     # 패스워드 일치하는지 검사
     if pw_receive == pwcheck_receive:
         pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
@@ -62,9 +62,6 @@ def sign_in():
         return jsonify({'result': 'success', 'token': token})
     else:  # 동일한 유저가 없으면,
         return jsonify({'result': 'fail', 'msg': '아이디/패스워드가 일치하지 않습니다.'})
-
-
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
